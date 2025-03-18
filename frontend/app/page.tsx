6045d82eafc271f27104dc24d686eb2a21bea92b
gokul <gokulkannant99@gmail.com>
2025-03-18 16:14:10 +0530

    minor fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import FileUpload from "@/app/components/FileUpload";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">Next.js File Upload</h1>
+      <h1 className="text-3xl font-bold text-gray-800 mb-6">Upload your .osm File</h1>
       <FileUpload />
     </main>
   );
